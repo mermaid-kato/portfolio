0fd265a024580b5837f59cc5e0a0816c1a45e11d
mermaid-kato <mermaid.kato@gmail.com>
2024-06-19 21:54:51 +0900

    経歴、プロフィールをそれぞれcomponent化

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,35 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { Portfolios } from './Portfolios';
+import { Careers } from './Careers';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <div className="App-portfolio">
-        <h1 className="Portfolio-title">Profile</h1>
-        <div className="Portfolio-name">まめかと（氏名）</div>
-        <div className="Portfolio-group">
-          <div className="Portfolio-image"></div>
-          <div className="Portfolio-group2">
-            <div className="Portfolio-birthday">生年月日：1998年10月19日</div>
-            <div className="Portfolio-language">使用言語：Ruby、Javascript、etc...</div>
-            <div className="Portfolio-thing">ひとこと：やる気だけはあります！！</div>
-          </div>
-        </div>
-      </div>
-      <div className="App-career">
-        <h1 className="Career-title">Career</h1>
-        <div className="Career-group">
-          <div className="Career-pj">199x年：xxxプロジェクトに従事</div>
-          <div className="Career-pj">199x年：xxxプロジェクトに従事</div>
-          <div className="Career-pj">199x年：xxxプロジェクトに従事</div>
-          <div className="Career-pj">199x年：xxxプロジェクトに従事</div>
-          <div className="Career-pj">199x年：xxxプロジェクトに従事</div>
-          <div className="Career-pj">199x年：xxxプロジェクトに従事</div>
-          <div className="Career-pj">199x年：xxxプロジェクトに従事</div>
-          <div className="Career-pj">199x年：xxxプロジェクトに従事</div>
-        </div>
-      </div>
+      <Portfolios/>
+      <Careers/>
     </div>
   );
 }
---
+++ frontend/app/src/Careers/index.js
@@ -0,0 +1,17 @@
+export const Careers = () => {
+  return (
+    <div className="App-career">
+      <h1 className="Career-title">Career</h1>
+      <div className="Career-group">
+        <div className="Career-pj">2001年：xxxプロジェクトに従事</div>
+        <div className="Career-pj">2002年：xxxプロジェクトに従事</div>
+        <div className="Career-pj">2003年：xxxプロジェクトに従事</div>
+        <div className="Career-pj">2004年：xxxプロジェクトに従事</div>
+        <div className="Career-pj">2005年：xxxプロジェクトに従事</div>
+        <div className="Career-pj">2006年：xxxプロジェクトに従事</div>
+        <div className="Career-pj">2007年：xxxプロジェクトに従事</div>
+        <div className="Career-pj">2008年：xxxプロジェクトに従事</div>
+      </div>
+    </div>
+  );
+};
