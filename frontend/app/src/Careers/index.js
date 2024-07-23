d8ceb26f03ed1b019419790020526f2e935e8398
mermaid-kato <mermaid.kato@gmail.com>
2024-07-24 00:20:15 +0900

    経歴をmapで表示（仮実装）

--- frontend/app/src/Careers/index.js
+++ frontend/app/src/Careers/index.js
@@ -1,18 +1,52 @@
 import styled from 'styled-components';
 
 export const Careers = () => {
+  const careerItems = [
+    {
+      year: "2000年",
+      title: "xxxプロジェクトに従事"
+    },
+    {
+      year: "2001年",
+      title: "xxxプロジェクトに従事"
+    },
+    {
+      year: "2002年",
+      title: "テストプロジェクトに従事"
+    },
+    {
+      year: "2003年",
+      title: "xxxプロジェクトに従事"
+    },
+    {
+      year: "2004年",
+      title: "ECサイトの設計・開発"
+    },
+    {
+      year: "2005年",
+      title: "ECサイトの設計・開発"
+    },
+    {
+      year: "2006年",
+      title: "施設予約プロジェクト"
+    },
+    {
+      year: "2007年",
+      title: "AWSの構築作業"
+    }
+  ];
+
   return (
     <AppCareer>
       <CareerTitle>Career</CareerTitle>
       <CareerGroup>
-        <CareerPj>2001年：xxxプロジェクトに従事</CareerPj>
-        <CareerPj>2001年：xxxプロジェクトに従事</CareerPj>
-        <CareerPj>2001年：xxxプロジェクトに従事</CareerPj>
-        <CareerPj>2001年：xxxプロジェクトに従事</CareerPj>
-        <CareerPj>2001年：xxxプロジェクトに従事</CareerPj>
-        <CareerPj>2001年：xxxプロジェクトに従事</CareerPj>
-        <CareerPj>2001年：xxxプロジェクトに従事</CareerPj>
-        <CareerPj>2001年：xxxプロジェクトに従事</CareerPj>
+      <>
+        {
+          careerItems.map((career) => {
+            return <CareerPj>{career.year}：{career.title}</CareerPj>
+          })
+        }
+      </>
       </CareerGroup>
     </AppCareer>
   );
