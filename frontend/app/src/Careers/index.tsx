b5bc2fd6f37e3692e5d0c6dd6b911ac07b93a183
mermaid-kato <mermaid.kato@gmail.com>
2024-10-28 03:31:52 +0900

    型定義

--- frontend/app/src/Careers/index.tsx
+++ frontend/app/src/Careers/index.tsx
@@ -23,7 +23,7 @@ export const Careers = () => {
     fetchData();
   }, []);
 
-  const careerItems = [
+  const careerItems: { year: string, title: string }[] = [
     {
       year: "2000年",
       title: "xxxプロジェクトに従事"
@@ -57,6 +57,39 @@ export const Careers = () => {
       title: "AWSの構築作業"
     }
   ];
+  //   {
+  //     year: "2000年",
+  //     title: "xxxプロジェクトに従事"
+  //   },
+  //   {
+  //     year: "2001年",
+  //     title: "xxxプロジェクトに従事"
+  //   },
+  //   {
+  //     year: "2002年",
+  //     title: "テストプロジェクトに従事"
+  //   },
+  //   {
+  //     year: "2003年",
+  //     title: "xxxプロジェクトに従事"
+  //   },
+  //   {
+  //     year: "2004年",
+  //     title: "ECサイトの設計・開発"
+  //   },
+  //   {
+  //     year: "2005年",
+  //     title: "ECサイトの設計・開発"
+  //   },
+  //   {
+  //     year: "2006年",
+  //     title: "施設予約プロジェクト"
+  //   },
+  //   {
+  //     year: "2007年",
+  //     title: "AWSの構築作業"
+  //   }
+  // ];
 
   return (
     <AppCareer>
